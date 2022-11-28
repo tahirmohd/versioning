b857217bfa6bf113d8be165d52a6e50187b797bc
tahir <t.mohamed.ebrahim@accenture.com>
2022-11-28 19:07:29 +0200

    first version

--- file1.js
+++ file1.js
@@ -1,1 +1,16 @@
-"version1"
+const person = {
+    fullName: function() {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+  const person1 = {
+    firstName:"John",
+    lastName: "Doe"
+  }
+  const person2 = {
+    firstName:"Mary",
+    lastName: "Doe"
+  }
+  
+  // This will return "John Doe":
+  person.fullName.call(person1);
